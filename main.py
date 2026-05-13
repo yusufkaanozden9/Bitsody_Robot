9a9b9d398daa913393e9cdd711ff16ca6bcdf448
yusufkaanozden9 <yusufkaanozden9@gmail.com>
2026-05-13 09:14:31 +0300

    Squashed 'g1-test/' content from commit ae8f1dae
    git-subtree-dir: g1-test
    git-subtree-split: ae8f1dae7486270787383751f70a590c7297d232

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+import sys
+
+from unitree_sdk2py.core.channel import ChannelPublisher, ChannelFactoryInitialize
+from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
+from unitree_sdk2py.idl.default import unitree_hg_msg_dds__LowCmd_
+from unitree_sdk2py.idl.default import unitree_hg_msg_dds__LowState_
+from unitree_sdk2py.idl.unitree_hg.msg.dds_ import LowCmd_
+from unitree_sdk2py.idl.unitree_hg.msg.dds_ import LowState_
+from unitree_sdk2py.utils.crc import CRC
+from unitree_sdk2py.utils.thread import RecurrentThread
+from unitree_sdk2py.comm.motion_switcher.motion_switcher_client import MotionSwitcherClient
+
+
+
+
+
+
+def main():
+    print("Hello from g1-test!")
+
+
+if __name__ == "__main__":
+    main()
